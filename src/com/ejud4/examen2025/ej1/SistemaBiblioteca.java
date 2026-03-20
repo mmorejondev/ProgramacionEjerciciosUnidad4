a38bb31a65247970d757fc0f7b537e56abb612bd
mmorejondev <mmorejondev@gmail.com>
2026-03-20 13:41:18 +0100

    Añadidos ejercicio 18 finalizado y examen de 2025 (éste último, sin terminar)

--- src/com/ejud4/examen2025/ej1/SistemaBiblioteca.java
+++ src/com/ejud4/examen2025/ej1/SistemaBiblioteca.java
@@ -1,5 +1,19 @@
 package com.ejud4.examen2025.ej1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaBiblioteca {
+
+	private List<Libro> listaLibros;
+	public SistemaBiblioteca() {
+		this.listaLibros = new ArrayList<>();
+	}
+
+	public void agregarLibro() {
+		
+	}
+
+
 
 }
